d993589f712f87e7ba519fc5b99d4a62b751470d
Gitdevtown <anshul@devtown.in>
2025-05-18 12:18:46 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Navbar from "./components/Navbar";
+import About from "./components/About";
+import Skill from "./components/Skill";
+export default function App() {
+  return (
+    <div>
+      <Navbar />
+      <About />
+      <Skill />
+    </div>
+  );
+}
